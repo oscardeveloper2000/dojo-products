9cd8a58170c76d71d70882d8f164b52dced72401
Oscardev <121882462+oscardeveloper2000@users.noreply.github.com>
2023-05-03 20:00:40 -0500

    Funcionalidad de eliminar productos del carrito y calcular total de compra en el carrito OK

--- src/app/modules/shared/components/shopping-cart/shopping-cart.component.ts
+++ src/app/modules/shared/components/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,7 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Product } from '@models/product.model';
+import { ShoppingCartService } from '@services/shopping-cart.service';
 interface InputData {
   products: Product[];
 }
@@ -13,18 +14,32 @@ interface OutputData {
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent {
+export class ShoppingCartComponent  implements OnInit{
   products: Product[] = [];
+  totalPrice: number = 0;
   constructor(
+    private shoppingCartService: ShoppingCartService,
     private dialogRef: DialogRef<OutputData>,
     @Inject(DIALOG_DATA) data: InputData
   ) {
     this.products = data.products;
+
     console.log('this.products :>> ', this.products);
+
+  }
+  ngOnInit(): void {
+    this.shoppingCartService.totalShoppingCart$
+    .subscribe((totalPrice) => {
+      this.totalPrice = totalPrice;
+    })
   }
 
   close() {
     this.dialogRef.close();
+  }
+
+  onRemoveProduct(product: Product){
+    this.shoppingCartService.removeProduct(product);
   }
 
 }
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -7,21 +7,30 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class ShoppingCartService {
 
+  totalPrice: number = 0;
+
   private myShoppingCart: Product[] = [];
   private myCart = new BehaviorSubject<Product[]>([]);
+  private totalShoppingCart = new BehaviorSubject<number>(0);
 
   myCart$ = this.myCart.asObservable();
+  totalShoppingCart$ = this.totalShoppingCart.asObservable();
 
   constructor() { }
 
   addProduct(product: Product){
     this.myShoppingCart.push(product);
     this.myCart.next(this.myShoppingCart);
+    this.getTotal();
   }
 
   removeProduct(product: Product){
     const index = this.myShoppingCart.findIndex(p => p.id === product.id);
     if (index !== -1) {
+      this.getTotal()
+      console.log('this.totalPrice :>> ', this.totalPrice);
+      console.log('product.price :>> ', product.price);
+      this.totalShoppingCart.next(this.totalPrice - product.price);
       this.myShoppingCart.splice(index, 1);
       this.myCart.next(this.myShoppingCart);
     }
@@ -33,6 +42,7 @@ export class ShoppingCartService {
   }
 
   getTotal() {
-    return this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.totalPrice = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.totalShoppingCart.next(this.totalPrice);
   }
 }
